a7993c1db6b06c5205c676d7e5d56b4804f5f9e2
Olivier Soucy <osoucy@zoho.com>
2021-08-10 18:05:27 -0400

    Mini fix of run_es_docker.sh.

--- travis/run_es_docker.sh
+++ travis/run_es_docker.sh
@@ -13,5 +13,5 @@ docker run \
   --network=esnet \
   --name=elasticsearch \
   --detach \
-  docker.elastic.co/elasticsearch/elasticsearch${ES_VERSION}
+  docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
 docker run --network esnet --rm appropriate/curl --max-time 120 --retry 120 --retry-delay 1 --retry-connrefused --show-error --silent http://elasticsearch:9200
