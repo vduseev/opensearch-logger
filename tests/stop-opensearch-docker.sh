c6bbb27552a83162fdc9541d769a896b20f3a9a3
vduseev <vagiz@duseev.com>
2025-08-13 12:01:26 +0200

    Bump version: 1.3.0 → 1.3.1

--- tests/stop-opensearch-docker.sh
+++ tests/stop-opensearch-docker.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker stop --time 3 test-opensearch-logger
+docker stop --timeout 3 test-opensearch-logger
